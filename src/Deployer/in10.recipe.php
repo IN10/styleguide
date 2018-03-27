c888db0d3443f097f2d09314e1477e4c0340fc3b
Jakob Buis <jakob.buis@in10.nl>
2018-03-27 13:56:19 +0200

    add the generic deployer recipe to the styleguide

---
+++ src/Deployer/in10.recipe.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Deployer;
+
+/*
+ * Deployer.org recipe for the IN10-specific build stack.
+ * We use a local build system: create an instance of the
+ * current project, build locally, and rsync all files to the server.
+ */
+
+// Based off the common laravel recipe
+require_once 'recipe/laravel.php';
+
+// Main, full process: make build -> release -> clean -> ??? -> profit!
+desc('Deploy your project');
+task('deploy', [
+    'build',
+    'release',
+    'cleanup',
+    'success'
+]);
+
+task('build', [
+    'build:create-folder',
+    'build:clone',
+    'build:prepare-env',
+    'build:backend',
+    'build:frontend',
+    'build:clean',
+    'build:version',
+]);
+
+task('build:create-folder', function () {
+    // Ensure the build directory exists, and is empty
+    within(__DIR__, function () {
+        run('rm -rf ./build');
+        run('mkdir ./build');
+    });
+})->local();
+
+task('build:clone', function () {
+    within(__DIR__.'/build', function () {
+        run('git clone ../ ./');
+    });
+})->local();
+
+task('build:prepare-env', function () {
+    within(__DIR__.'/build', function () {
+        run('cp ../.env ./.env');
+    });
+})->local();
+
+task('build:backend', function () {
+    within(__DIR__.'/build', function () {
+        run('composer install --no-interaction --prefer-dist --optimize-autoloader');
+    });
+})->local();
+
+task('build:frontend', function () {
+    within(__DIR__.'/build', function () {
+        run('yarn build');
+    });
+})->local();
+
+task('build:clean', function () {
+    within(__DIR__.'/build', function () {
+        run('rm -rf node_modules');
+        run('rm -rf .git');
+        run('rm -rf .env');
+        run('rm -rf resources/assets/*');
+        run('rm -rf tests/*');
+    });
+})->local();
+
+task('build:version', function () {
+    within(__DIR__.'/build', function () {
+        $sha = run('git rev-parse HEAD');
+        $username = run('whoami');
+        $date = run('date +"%Y-%m-%d @ %H:%M:%S"');
+        run("echo '$sha - $date - $username' >> .version");
+    });
+})->local();
+
+// Make a release
+task('release', [
+    'deploy:info',
+    'deploy:lock',
+    'deploy:release',
+    'upload',
+    'deploy:shared',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:clear',
+    'artisan:cache:clear',
+    'artisan:config:cache',
+    'artisan:optimize',
+    'artisan:migrate',
+    'deploy:symlink',
+    'deploy:unlock',
+]);
+
+// Upload files to server
+task('upload', function () {
+    upload(__DIR__ . '/build/', '{{release_path}}');
+});
+
+// Auto-unlock when the deployment fails
+after('deploy:failed', 'deploy:unlock');
