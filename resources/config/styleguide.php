43b06a7c51ca045faecf32bc71a24d96aeb1f78f
Jakob Buis <jakob.buis@in10.nl>
2018-07-13 11:22:05 +0200

    add configuration option styleguide.in_styleguide to detect whether we are currently on a styleguide path

--- resources/config/styleguide.php
+++ resources/config/styleguide.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    // This variable toggles whether we are currently on a styleguide route
+    // or in the main website
+    'in_styleguide' => false,
 
     'view_path' => 'resources/views/styleguide',
-
 ];
--- src/Controllers/StyleguideController.php
+++ src/Controllers/StyleguideController.php
@@ -3,6 +3,7 @@
 namespace IN10\Styleguide\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use IN10\Styleguide\Services\Views;
 
 class StyleguideController
@@ -14,6 +15,9 @@ class StyleguideController
      */
     public function index()
     {
+        // Set the styleguide interpretation flag
+        Config::set('styleguide.in_styleguide', true);
+
         $sections = (new Views())->read();
 
         return view('styleguide::dashboard', compact('sections'));
@@ -28,6 +32,9 @@ class StyleguideController
      */
     public function show($id)
     {
+        // Set the styleguide interpretation flag
+        Config::set('styleguide.in_styleguide', true);
+
         return view(str_replace("resources/views", "", config('styleguide.view_path')).".$id");
     }
 }
